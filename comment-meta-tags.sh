baf7b48e4b7399a3f195f0c69bac7371775dcde4
straysheep-dev <bah@straysheep.dev>
2025-12-31 09:37:24 -0800

    Create script to handle meta tags

---
+++ comment-meta-tags.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# GPL-3.0 License
+
+# This cripts comments out all "meta" tags in the tags-all.txt file.
+# These tags are usually too broad, or can be selected easily using
+# the regex below for specific compliance requirements.
+# aide and auditd tags are also excluded due to the higher failure
+# rate.
+
+meta_tags='^always$
+^aide_.*$
+^audit_.*$
+^CJIS-.*$
+^.*_strategy$
+^DISA-.*$
+^high_.*$
+^medium_.*$
+^low_.*$
+^unknown_.*$
+^NIST-.*$
+^.*reboot_.*$
+^PCI-.*$'
+
+while IFS= read -r tag; do
+    echo "[*]Disabling ${tag}..."
+    sed -E -i "s/(${tag})/#\1/g" tags-all.txt
+done <<< "${meta_tags}"
