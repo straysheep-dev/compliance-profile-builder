98270310a5c02df97ee129fce90efffc1fe31233
straysheep-dev <bah@straysheep.dev>
2025-12-31 09:36:13 -0800

    Update scan and remediation scripts

--- make-remediation.sh
+++ make-remediation.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-results_file='/path/to/results.xml'
-result_id='<xccdf_org.open-scap_testresult_xccdf_org.ssgproject.content_profile_name>'
+
+# GPL-3.0 License
+
+# This script will ingest the results.xml file from an oscap scan
+# and build a remediation playbook based on the fix_type variable
+# below (default is ansible).
+
+if ! command -v oscap > /dev/null; then
+    echo "[*]oscap not installed."
+    exit 1
+fi
+
+# Replace this variable with the SCAP profile you're using
+result_id=$1
+
+if [[ $result_id == '' ]]; then
+    echo "[*] Specify a SCAP profile ID."
+    echo ""
+    echo "    Example: xccdf_org.open-scap_testresult_xccdf_org.ssgproject.content_profile_name"
+    echo ""
+    exit 1
+fi
+
+# Vars
+scan_folder="$(pwd)/scans"
+results_file="$scan_folder/results.xml"
 fix_type='ansible'
-remediation_file='/path/to/write/your/remediation.file'
+remediation_file="$scan_folder/remediation.file"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$scan_folder" || exit 1
+
+# Build the playbook
 oscap xccdf generate fix --fix-type "$fix_type" --output "$remediation_file" --result-id "$result_id" "$results_file"
--- scan.sh
+++ scan.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-results_path='/path/to/results.xml'
-report_path='/path/to/report.html'
-profile_id='<xccdf_org.ssgproject.content_profile_some_profile>'
-ssg_data_file='/path/to/<ssg_data_file>.xml'
+
+# GPL-3.0 License
+
+# This script will scan the local machine with oscap using the profile specified.
+
+if ! command -v oscap > /dev/null; then
+    echo "[*]oscap not installed."
+    exit 1
+fi
+
+# Replace these variables with the SCAP profile and data file you're using
+profile_id="$1"
+ssg_data_file="$2"
+
+if [[ $profile_id == '' ]]; then
+    echo "[*] Specify a SCAP profile ID."
+    echo ""
+    echo "    Example: xccdf_org.ssgproject.content_profile_some_profile"
+    echo ""
+    exit 1
+fi
+
+if [[ $ssg_data_file == '' ]]; then
+    echo "[*] Specify a SCAP data file."
+    echo ""
+    echo "    Example: /path/to/<ssg_data_file>.xml"
+    echo ""
+    exit 1
+fi
+
+# Vars
+scan_folder="$(pwd)/scans"
+results_path="$scan_folder/results.xml"
+report_path="$scan_folder/report.html"
+
+# Run a scan
 oscap xccdf eval --profile "$profile_id" --results-arf "$results_path" --report "$report_path" "$ssg_data_file"
