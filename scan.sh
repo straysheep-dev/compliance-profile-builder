c2e4577b6da3db17cec0a7b78ac19bc1a4fd2e26
straysheep-dev <bah@straysheep.dev>
2026-01-01 09:42:02 -0800

    Update scan script to work locally and remotely

--- scan.sh
+++ scan.sh
@@ -4,14 +4,33 @@
 
 # This script will scan the local machine with oscap using the profile specified.
 
+# shellcheck source=/dev/null
+source /etc/os-release
+
 if ! command -v oscap > /dev/null; then
     echo "[*]oscap not installed."
+    echo "    dnf install openscap-scanner"
+    echo "    apt install libopenscap8"
     exit 1
 fi
 
+if ! [[ "$ID" == 'debian' ]] && ! [[ "$ID" == 'ubuntu' ]]; then
+    if ! command -v oscap-ssh > /dev/null; then
+        echo "[*]openscal-utils not installed."
+        exit 1
+    fi
+fi
+
 # Replace these variables with the SCAP profile and data file you're using
-profile_id="$1"
-ssg_data_file="$2"
+scan_target="$1"
+profile_id="$2"
+ssg_data_file="$3"
+
+if [[ $scan_target == '' ]]; then
+    echo "[*] Specify either 'localhost' or 'user@remote port'. Note the space between remote and port."
+    echo ""
+    exit 1
+fi
 
 if [[ $profile_id == '' ]]; then
     echo "[*] Specify a SCAP profile ID."
@@ -34,5 +53,13 @@ scan_folder="$(pwd)/scans"
 results_path="$scan_folder/results.xml"
 report_path="$scan_folder/report.html"
 
+if ! [[ -d "$scan_folder" ]]; then
+    mkdir -p "$scan_folder" || exit 1
+fi
+
 # Run a scan
-oscap xccdf eval --profile "$profile_id" --results-arf "$results_path" --report "$report_path" "$ssg_data_file"
+if [[ "$scan_target" == "localhost" ]]; then
+    sudo oscap xccdf eval --profile "$profile_id" --results-arf "$results_path" --report "$report_path" "$ssg_data_file"
+else
+    oscap-ssh --sudo "$1" xccdf eval --profile "$profile_id" --results-arf "$results_path" --report "$report_path" "$ssg_data_file"
+fi
